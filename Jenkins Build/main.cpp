0903616786f0bea1ae827e5e938202e021b8def2
moyakino <104491816+moyakino@users.noreply.github.com>
2026-01-13 11:51:43 +0900

    change

--- Jenkins Build/main.cpp
+++ Jenkins Build/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Add: " << add(10, 2) << endl;
+	cout << "Add: " << add(10, 100) << endl;
 	cout << "Sub: " << sub(10, 3) << endl;
 	cout << "Mul: " << mul(10, 4) << endl;
 	cout << "Div: " << divi(10, 5) << endl;
